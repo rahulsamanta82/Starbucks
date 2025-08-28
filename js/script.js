26859acc6bdded5e7590d38c4cbeb7fddb23fea5
rahulsamanta82 <rs4655742@gmail.com>
2025-08-28 21:08:37 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 function imgSlider(anything){
   document.querySelector('.starbucks').src = anything;
 }
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
